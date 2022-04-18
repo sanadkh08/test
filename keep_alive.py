30902dfe934062bb9f642c15c7e57c11bf300727
SDK <79312444+sanadkh08@users.noreply.github.com>
2022-04-18 17:55:20 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,1 @@
-from concurrent.futures import thread
-from flask import Flask
-from threading import Thread
 
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "IAM TURNED ON NIGGA"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
